0b48482399b562a01174e2f4327024368b0c07d9
baolinhdev <baolinhdev@gmail.com>
2024-11-17 15:58:39 +0700

    Render Array

--- App.tsx
+++ App.tsx
@@ -1,40 +1,45 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState<string>("");
-  const [age, setAge] = useState<number>(0);
+  const [students, setStudents] = useState([
+    { id: 1, name: "Bao Linh", age: 19 },
+    { id: 2, name: "Minh Tu", age: 21 },
+    { id: 3, name: "Nguyen Hoang", age: 20 },
+    { id: 4, name: "Lan Anh", age: 22 },
+    { id: 5, name: "Tuan Anh", age: 18 },
+    { id: 6, name: "Mai Lan", age: 23 },
+    { id: 7, name: "Thao Ly", age: 20 },
+    { id: 8, name: "Duy Anh", age: 22 },
+    { id: 9, name: "Hieu Hoang", age: 19 },
+    { id: 10, name: "Khanh Linh", age: 21 },
+    { id: 11, name: "Hieu Minh", age: 22 }, // Thêm nhiều sinh viên
+    { id: 12, name: "Thanh Mai", age: 20 },
+    { id: 13, name: "Lan Thanh", age: 18 },
+    { id: 14, name: "Tuan Ki", age: 21 },
+    { id: 15, name: "Tuan Hoang", age: 20 },
+  ]);
+
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.text}>Age: {age}</Text>
-        <TextInput
-          multiline
-          onChangeText={(value) => setAge(Number(value))}
-          style={{
-            borderColor: "green",
-            borderWidth: 1,
-            width: 200,
-            padding: 15,
-          }}
-          keyboardType="numeric"
-        />
-      </View>
-      <View>
-        <Text style={styles.text}>Name: {name}</Text>
-        <TextInput
-          multiline
-          onChangeText={(value) => setName(value)}
-          style={{
-            borderColor: "green",
-            borderWidth: 1,
-            width: 200,
-            padding: 15,
-          }}
-        />
-      </View>
-    </View>
+    <ScrollView style={{ flex: 1 }} >
+      {students.map((student) => {
+        return (
+          <View
+            key={student.id}
+            style={{
+              padding: 20,
+              backgroundColor: "green",
+              marginBottom: 40,
+            }}
+          >
+            <Text style={styles.text}>{student.id}</Text>
+            <Text style={styles.text}>{student.name}</Text>
+            <Text style={styles.text}>{student.age}</Text>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
 }
 
